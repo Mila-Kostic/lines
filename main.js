ac7a372acac555a95c3a21e5e2d45aba65f4c722
Mila Kostic <milakostic.eng@gmail.com>
2022-08-31 07:21:00 +0200

    random ball

--- main.js
+++ main.js
@@ -1,7 +1,26 @@
 let container = document.querySelector(".container");
-
+let ballColors = [
+  "red",
+  "green",
+  "blue",
+  "yellow",
+  "indianred",
+  "skyblue",
+  "gray",
+];
 makeGrid();
 
+let boxes = document.querySelectorAll(".box");
+
+makeBalls(10);
+
+function makeBalls(ballNumber) {
+  let rand = Math.floor(Math.random() * boxes.length);
+  let rand2 = Math.floor(Math.random() * ballColors.length);
+  let randColor = ballColors[rand2];
+  let randBox = boxes[rand];
+  randBox.innerHTML = `<div class="ball" style="background: ${randColor}"></div>`;
+}
 function makeGrid() {
   let text = ``;
   for (let i = 0; i < 100; i++) {
@@ -11,3 +30,5 @@ function makeGrid() {
   }
   container.innerHTML = text;
 }
+
+/**7 min */
